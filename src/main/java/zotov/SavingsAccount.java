31ee9c686be8cf964d496187f29d26da20abfef8
Ekaterina Zotova <139338863+zookatt@users.noreply.github.com>
2026-07-30 12:40:25 +0200

    fix(SavingsAccount): delete commented lines

--- src/main/java/zotov/SavingsAccount.java
+++ src/main/java/zotov/SavingsAccount.java
@@ -44,19 +44,4 @@ public class SavingsAccount extends Account {
                 "\nMonthly fee: " + monthlyFee +
                 "\nNumber of transactions: " + transactions;
     }
-
-    // Cuenta de ahorros: posee un atributo para determinar si la cuenta de ahorros
-    // está activa (tipo boolean). Si el saldo es menor a $10000, la cuenta está
-    // inactiva, en caso contrario se considera activa. Los siguientes métodos se
-    // redefinen:
-    // Consignar: se puede consignar dinero si la cuenta está activa. Debe invocar
-    // al método heredado.
-    // Retirar: es posible retirar dinero si la cuenta está activa. Debe invocar al
-    // método heredado.
-    // Extracto mensual: si el número de retiros es mayor que 4, por cada retiro
-    // adicional, se cobra $1000 como comisión mensual. Al generar el extracto, se
-    // determina si la cuenta está activa o no con el saldo.
-    // Un nuevo método imprimir que retorna el saldo de la cuenta, la comisión
-    // mensual y el número de transacciones realizadas (suma de cantidad de
-    // consignaciones y retiros).
 }
