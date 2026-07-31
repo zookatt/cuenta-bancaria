a4013ca4521cb249f7ccbbc934b1616802ad7f0a
zookat <zotova.ea@gmail.com>
2026-07-31 10:20:53 +0200

    feat: (CurrentAccount): add new print method

--- src/main/java/zotov/CurrentAccount.java
+++ src/main/java/zotov/CurrentAccount.java
@@ -36,19 +36,12 @@ public class CurrentAccount extends Account {
         super.calculateMonthlyStatement();
     }
 
+    public String print() {
+        int numberOfTransactions = numberOfDeposits + numberOfWithdrawals;
+        return "Balance: " + balance +
+                "\nMonthly fee: " + monthlyFee +
+                "\nNumber of transactions: " + numberOfTransactions +
+                "\nOverdraft: " + overdraft;
+    }
+
 }
-// Cuenta corriente: posee un atributo de sobregiro,
-// el cual se inicializa en cero.
-
-// Se redefinen los siguientes métodos:
-// Retirar: se retira dinero de la cuenta actualizando su saldo.
-// Se puede retirar dinero superior al saldo. El dinero
-// que se debe queda como sobregiro.
-
-// Consignar: invoca al método heredado. Si hay sobregiro,
-// la cantidad consignada reduce el sobregiro.
-
-// Extracto mensual: invoca al método heredado.
-// Un nuevo método imprimir que retorna el saldo de la cuenta,
-// la comisión mensual, el número de transacciones realizadas
-// (suma de cantidad de consignaciones y retiros) y el valor de sobregiro.
